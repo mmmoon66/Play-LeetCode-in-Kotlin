64c94414e6b507e8948151db734f5a7cb582a34c
guoliang03 <guoliang03@kuaishou.com>
2020-01-20 09:44:20 +0800

    121. Best Time to Buy and Sell Stock

---
+++ 0121-Best-Time-to-Buy-and-Sell-Stock/Solution0121.kt
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+// 121. Best Time to Buy and Sell Stock
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Note that you cannot sell a stock before you buy one.
+
+Example 1:
+
+Input: [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+             Not 7-1 = 6, as selling price needs to be larger than buying price.
+Example 2:
+
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ */
+class Solution0121 {
+    fun maxProfit(prices: IntArray): Int {
+        if (prices.size < 2) return 0
+        var min = prices[0]
+        var res = 0
+        for (i in 1 until prices.size) {
+            res = maxOf(res, prices[i] - min)
+            min = minOf(min, prices[i])
+        }
+        return res
+    }
+}
+
+fun main() {
+    val s = Solution0121()
+    println(s.maxProfit(intArrayOf(7, 1, 5, 3, 6, 4)))
+    println(s.maxProfit(intArrayOf(7, 6, 4, 3, 1)))
+}
+/*
+Runtime: 136 ms, faster than 99.28% of Kotlin online submissions for Best Time to Buy and Sell Stock.
+Memory Usage: 41.6 MB, less than 100.00% of Kotlin online submissions for Best Time to Buy and Sell Stock.
+ */
