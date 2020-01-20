0db658fb1a9afd113812f12c860b55cc4d0d2cc0
guoliang03 <guoliang03@kuaishou.com>
2020-01-20 13:20:50 +0800

    543. Diameter of Binary Tree

---
+++ 0543-Diameter-of-Binary-Tree/Solution0543.kt
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/diameter-of-binary-tree/
+// 543. Diameter of Binary Tree
+/*
+Given a binary tree, you need to compute the length of the diameter of the tree. The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+Example:
+Given a binary tree
+          1
+         / \
+        2   3
+       / \
+      4   5
+Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+
+Note: The length of path between two nodes is represented by the number of edges between them.
+ */
+class Solution0543 {
+    private var max = 0
+
+    fun depthOfBinaryTree(root: TreeNode?): Int {
+        if (root == null) {
+            return 0
+        }
+        val leftDepth = depthOfBinaryTree(root.left)
+        val rightDepth = depthOfBinaryTree(root.right)
+        max = maxOf(max, leftDepth + rightDepth)
+        return 1 + maxOf(leftDepth, rightDepth)
+    }
+
+    fun diameterOfBinaryTree(root: TreeNode?): Int {
+        max = 0
+        depthOfBinaryTree(root)
+        return max
+    }
+}
+/*
+Runtime: 132 ms, faster than 100.00% of Kotlin online submissions for Diameter of Binary Tree.
+Memory Usage: 41.9 MB, less than 100.00% of Kotlin online submissions for Diameter of Binary Tree.
+ */
