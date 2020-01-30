ccc1057773baeddb8fa5b5a849d972b7a806b0fb
guoliang03 <guoliang03@kuaishou.com>
2020-01-31 04:24:46 +0800

    48. Rotate Image

---
+++ 0048-Rotate-Image/Solution0048.kt
@@ -0,0 +1,67 @@
+import com.sun.deploy.ui.MacJavaTrayIcon
+
+// https://leetcode.com/problems/rotate-image/
+// 48. Rotate Image
+/*
+You are given an n x n 2D matrix representing an image.
+
+Rotate the image by 90 degrees (clockwise).
+
+Note:
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+Example 1:
+Given input matrix =
+[
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+],
+rotate the input matrix in-place such that it becomes:
+[
+  [7,4,1],
+  [8,5,2],
+  [9,6,3]
+]
+
+Example 2:
+Given input matrix =
+[
+  [ 5, 1, 9,11],
+  [ 2, 4, 8,10],
+  [13, 3, 6, 7],
+  [15,14,12,16]
+],
+rotate the input matrix in-place such that it becomes:
+[
+  [15,13, 2, 5],
+  [14, 3, 4, 1],
+  [12, 6, 8, 9],
+  [16, 7,10,11]
+]
+ */
+class Solution0048 {
+    fun rotate(matrix: Array<IntArray>): Unit {
+        // 先上下翻转
+        val n = matrix.size
+        var i = 0
+        var j = n - 1
+        while(i < j) {
+            matrix[i] = matrix[j].also { matrix[j] = matrix[i] }
+            ++i
+            --j
+        }
+        // 再沿左上到右下的对角线翻转
+        for (u in 0 until n) {
+            for (v in 0 until n) {
+                if (u > v) {
+                    matrix[u][v] = matrix[v][u].also { matrix[v][u] = matrix[u][v] }
+                }
+            }
+        }
+    }
+}
+/*
+Runtime: 140 ms, faster than 91.38% of Kotlin online submissions for Rotate Image.
+Memory Usage: 37.5 MB, less than 100.00% of Kotlin online submissions for Rotate Image.
+ */
