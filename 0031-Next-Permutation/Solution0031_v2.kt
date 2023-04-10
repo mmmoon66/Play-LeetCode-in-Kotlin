f76b6b8a8a7b8558ea550b665f7f093419c229f9
edward <llxxla1992@gmail.com>
2023-04-10 23:45:11 +0800

    update

--- 0031-Next-Permutation/Solution0031_v2.kt
+++ 0031-Next-Permutation/Solution0031_v2.kt
@@ -1,5 +1,3 @@
-import java.util.*
-
 // https://leetcode.com/problems/next-permutation/
 // 31. Next Permutation
 /*
@@ -18,63 +16,36 @@ Here are some examples. Inputs are in the left-hand column and its corresponding
 class Solution0031_v2 {
     fun nextPermutation(nums: IntArray) {
         var i = nums.lastIndex - 1
-        while(i >= 0 && nums[i] >= nums[i + 1]) --i
+        // 从后向前找到第一个升序对
+        while (i >= 0 && nums[i] >= nums[i + 1]) --i
         if (i >= 0) {
+            // 从后向前找到第一个比nums[i]大的元素
             var j = nums.lastIndex
-            while(j >= i + 1 && nums[j] <= nums[i]) --j
+            while (j >= i + 1 && nums[j] <= nums[i]) --j
+            // 交换nums[i]和nums[j]
             nums[i] = nums[j].also { nums[j] = nums[i] }
         }
+        // 可以证明，交换完nums[i]和nums[j]后，nums[i+1..lastIndex]是降序的，需要将这个区间的元素reverse成升序的
         reverse(nums, i + 1, nums.lastIndex)
     }
 
-    private fun reverse(arr: IntArray, left: Int, right: Int) {
-        assert(left >= 0 && right < arr.size)
-        var i = left
-        var j = right
-        while(i < j) {
-            arr[i] = arr[j].also { arr[j] = arr[i] }
-            ++i
-            --j
-        }
-    }
-
-    fun nextPermutation2(nums: IntArray) {
-        // 从后向前找到第一个顺序对(i, i+1),满足nums[i] < nums[i+1]
-        var i = nums.lastIndex - 1
-        while(i >= 0 && nums[i] >= nums[i + 1]) --i
-        if (i < 0) {
-            // 没有找到顺序对 => nums是降序数组
-            var j = 0
-            var k = nums.lastIndex
-            while(j < k) {
-                nums[j] = nums[k].also { nums[k] = nums[j] }
-                ++j
-                --k
-            }
-        } else {
-            // 找到顺序对
-            // 在区间 nums[i+1,lastIndex] 内从后向前查找第一个大于nums[i]的元素nums[j]
-            var j = nums.lastIndex
-            while(j >= i + 1 && nums[j] <= nums[i]) --j
-            // 交换nums[i]和nums[j]
-            nums[i] = nums[j].also { nums[j] = nums[i] }
-            // 交换之后，可以证明 nums[i+1,lastIndex]是降序的，将此区间的元素反转一下就能得到最终结果
-            var m = i + 1
-            var n = nums.lastIndex
-            while(m < n) {
-                nums[m] = nums[n].also { nums[n] = nums[m] }
-                ++m
-                --n
-            }
+    private fun reverse(nums: IntArray, i: Int, j: Int) {
+        assert(i >= 0 && j < nums.size)
+        var p = i
+        var q = j
+        while (p < q) {
+            nums[p] = nums[q].also { nums[q] = nums[p] }
+            ++p
+            --q
         }
     }
 }
 
 fun main() {
     val s = Solution0031_v2()
-    val nums = intArrayOf(1, 2)
+    val nums = intArrayOf(1, 3, 2)
     s.nextPermutation(nums)
-    println("size:${nums.size}, contents:${nums.joinToString()}")
+    println(nums.joinToString())
 }
 /*
 执行用时：260 ms, 在所有 Kotlin 提交中击败了15.62%的用户
