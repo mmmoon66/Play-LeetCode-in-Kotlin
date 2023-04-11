0c0e6f37ba20c7989df2804687ff22f580460eb5
edward <llxxla1992@gmail.com>
2023-04-11 17:43:08 +0800

    update

--- 0022-Generate-Parentheses/Solution0022.kt
+++ 0022-Generate-Parentheses/Solution0022.kt
@@ -14,15 +14,17 @@ For example, given n = 3, a solution set is:
 ]
  */
 class Solution0022 {
-    fun generateParenthesis(n: Int): List<String> {
+
+    // 方法1: 动态规划
+/*    fun generateParenthesis(n: Int): List<String> {
         val dp = mutableListOf(listOf(""))
         for (i in 1..n) {
-            // (j)i-1-j
-            // 括号内j对括号，括号外(i - 1 - j)对括号
             val list = mutableListOf<String>()
-            for (j in 0 until i) {
-                dp[j].forEach { str1 ->
-                    dp[i - 1 - j].forEach { str2 ->
+            // ({countIn对括号}){countOut对括号}
+            for (countIn in 0 until i) {
+                val countOut = i - 1 - countIn
+                dp[countIn].forEach { str1 ->
+                    dp[countOut].forEach { str2 ->
                         list.add("($str1)$str2")
                     }
                 }
@@ -30,12 +32,33 @@ class Solution0022 {
             dp.add(list)
         }
         return dp.last()
+    }*/
+
+    // 方法2: 记忆化搜索
+    private val memo = mutableListOf(listOf(""))
+    fun generateParenthesis(n: Int): List<String> {
+        memo.getOrNull(n)?.let {
+            return it
+        }
+        // ({countIn对括号}){countOut对括号}
+        val ret = mutableListOf<String>()
+        for (countIn in 0 until n) {
+            val countOut = n - 1 - countIn
+            generateParenthesis(countIn).forEach { str1 ->
+                generateParenthesis(countOut).forEach { str2 ->
+                    ret.add("($str1)$str2")
+                }
+            }
+        }
+        memo.add(ret)
+        return ret
     }
 }
 
 fun main() {
     val s = Solution0022()
     println(s.generateParenthesis(3))
+    println(s.generateParenthesis(2))
 }
 /*
 Runtime: 136 ms, faster than 89.13% of Kotlin online submissions for Generate Parentheses.
