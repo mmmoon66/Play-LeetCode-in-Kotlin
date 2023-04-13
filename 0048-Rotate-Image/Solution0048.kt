d3af799c419c9c3d602f1e386690d373decb5079
edward <llxxla1992@gmail.com>
2023-04-13 12:10:14 +0800

    update

--- 0048-Rotate-Image/Solution0048.kt
+++ 0048-Rotate-Image/Solution0048.kt
@@ -40,24 +40,42 @@ rotate the input matrix in-place such that it becomes:
  */
 class Solution0048 {
     fun rotate(matrix: Array<IntArray>): Unit {
-        // 先上下翻转
+        // [1,2,3]
+        // [4,5,6]
+        // [7,8,9]
+        // 先上下翻转得到
+        // [7,8,9]
+        // [4,5,6]
+        // [1,2,3]
         val n = matrix.size
+        if (n <= 1) return
         var i = 0
         var j = n - 1
-        while(i < j) {
+        while (i < j) {
             matrix[i] = matrix[j].also { matrix[j] = matrix[i] }
             ++i
             --j
         }
-        // 再沿左上到右下的对角线翻转
+        // 再沿着左上到右下的对角线翻转
+        // [7,4,1]
+        // [8,5,2]
+        // [9,6,3]
         for (u in 0 until n) {
-            for (v in 0 until n) {
-                if (u > v) {
-                    matrix[u][v] = matrix[v][u].also { matrix[v][u] = matrix[u][v] }
-                }
+            for (v in 0 until u) {
+                matrix[u][v] = matrix[v][u].also { matrix[v][u] = matrix[u][v] }
             }
         }
     }
+}
+
+fun main() {
+    val matrix = arrayOf(
+        intArrayOf(1, 2, 3),
+        intArrayOf(4, 5, 6),
+        intArrayOf(7, 8, 9)
+    )
+    println(Solution0048().rotate(matrix))
+    matrix.forEach { println(it.contentToString()) }
 }
 /*
 Runtime: 140 ms, faster than 91.38% of Kotlin online submissions for Rotate Image.
